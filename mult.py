87211b0e4bf96e0b59729654dffb28b83c926af3
Your Name <sandhya06012000@gmail.com>
2021-06-25 22:11:49 +0530

    multipication

---
+++ mult.py
@@ -0,0 +1,13 @@
+m1 = int(input("enter the number "))
+m2 = int(input("enter the number "))
+m3 = int(input("enter the number "))
+multi = m1*m2*m3
+print(multi) 
+
+
+
+m1 = 10
+m2 = 20
+m3 = 90
+m = m1*m2*m3
+print(m)
